902ba12903a9aede64f56ff90b627fa28cec8de7
Jeremy Tollenaers <jtollena@fu-r3-p5.s19.be>
2024-01-23 13:56:49 +0100

    save

---
+++ simple_cmd.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   simple_cmd.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jtollena <jtollena@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/22 13:04:25 by jtollena          #+#    #+#             */
+/*   Updated: 2024/01/22 13:38:42 by jtollena         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int	execute_simple_cmd(char *cmd, char **argv)
+{
+	execve(cmd, argv, NULL);
+	return 0;
+}
+
+int	main(int argc, char **argv)
+{
+	execute_simple_cmd(argv[1], &argv[1]);
+	while (1)
+	{
+		pause();
+	}
+	
+	return 0;
+}
